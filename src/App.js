ab936e38839a4e103221f29a653de79ecbbcc254
wenhai03 <810068733@qq.com>
2019-07-19 12:23:13 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.styl';
+import './less.less'
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+        <div className='App-header'>
+          这里是stylus
+        </div>
+
+        <div className='less'>
+          这里是less
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
